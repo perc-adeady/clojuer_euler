451706686c99fa7435f264d5f19b956c3a3f1ad1
Angela Deady <angela.deady@perceptivesoftware.com>
2014-12-18 13:27:38 -0500

    let loop cleanup

--- euler_1/src/euler_1/core.clj
+++ euler_1/src/euler_1/core.clj
@@ -2,59 +2,28 @@
   (:gen-class))
 
 
-(defn multiple_3?
-      [i]
-      (zero? (rem i 3))
-      )
-
-(defn multiple_5?
-      [i]
-      (zero? (rem i 5))
-      )
-
 (defn add_to_sum?
+      "add numbers that are multiples of 3 or 5"
       [i]
-      (or (multiple_3? i) (multiple_5? i) )
+      (or (zero? (rem i 3)) (zero? (rem i 5)) )
 
       )
 
-(defn inc_sum
-      [current sum]
-      (if (add_to_sum? current)
-        (+ sum current)
-        )
-
-  )
-
-(defn id
-      [i]
-      i)
-
-(defn my_function
+(defn sum_mutliples_of_3_and_5
   [args sum]
 
-  (def retVal
-    (let [[current & rest] args]
-         (if rest
-           (if (add_to_sum? current)
-             (do
-               (my_function rest (inc_sum current sum))
-               )
-             (do
-               (my_function rest sum)
-               )
-             )
-           (id sum)
+  (let [[current & rest] args]
+       (if rest
+         (if (add_to_sum? current)
+             (sum_mutliples_of_3_and_5 rest (+ sum current))
+             (sum_mutliples_of_3_and_5 rest sum)
            )
+         sum
          )
-    )
-
-  retVal
-
+       )
   )
-
 
 (defn -main
   [& args]
-  (println (my_function [1 2 3 4 5 6 7 8 9 10] 0))
+  (println (sum_mutliples_of_3_and_5 (range 1000) 0))
 )
